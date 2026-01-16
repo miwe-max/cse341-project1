71e31709f9676e51b5b6a15764c4d4b1502bdd63
miwe-max <miwe@byupathway.edu>
2026-01-16 15:09:40 +0100

    added the connection to mongodb

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,2 +1,29 @@
 const express = require('express');
 const router = express.Router();
+// const database = require('../data/database');
+
+// router.get('/contacts', async (req, res) => {
+//     let client;
+//     try{
+//         client = await database.initDb();
+//         const db = client.db('cse341-project');
+//         const collection = db.collection('contacts');
+//         const data = await collection.find({}).toArray();
+//         res.json(data);
+//     } catch (error) {
+//         console.error('Error fetching data:', error);
+//         res.status(500).send('An error occured while fetching data');
+//     } finally {
+//         if(client) {
+//             await client.close();
+//         }
+//     }
+// });
+
+// module.exports = router;
+
+const contactsController = require('../controllers/contacts');
+router.get('/', contactsController.getAll);
+router.get('/:id', contactsController.getSingle);
+
+module.exports = router;
