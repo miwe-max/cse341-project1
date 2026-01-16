cebdf7faef470415135cfef6aae44699d11e5193
miwe-max <miwe@byupathway.edu>
2026-01-16 13:50:44 +0100

    refractorered initial commit hello world

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
 const express = require('express');
 const app = express();
+const port = process.env.PORT || 8080;
 
-const PORT = process.env.PORT || 3000;
+app.use('/', require('./routes')); 
 
-app.use('/', require('./routes'));
-
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+app.listen(port, () => {
+    console.log(`Web server is listening at port ${port}`);
 });
