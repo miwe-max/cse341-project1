31cef9949db9a839c4a2b2f67e8814b331c7e1aa
Maxwell Iwe <miwe@byupathway.edu>
2026-01-16 13:38:08 +0100

    Refactor routes to use a single express Router
    edited initial commit

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,8 @@
-const { Router } = require('express');
+const express = require('express');
+const router = express.Router();
 
-const routes = require('express').Router();
-
-routes.get('/', (req, res) => {
-    res.send('Welcome to the Home Page');
+router.get('/', (req, res) => {
+    res.send('Hello World');
 });
 
-module.exports = routes;
+module.exports = router;
